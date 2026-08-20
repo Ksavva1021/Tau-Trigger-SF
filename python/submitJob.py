70df45f4261a47b69b93a6e11c8b5ccfd45e44e6
Dougal <dougalwentworth@gmail.com>
2026-08-20 15:49:44 +0100

    Added condor submission functionality to nano_postproc.py

---
+++ python/submitJob.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def condor_submit(commands, condor_dir, job_name):
+    # Create executable shell script for the command
+    with open(f"{condor_dir}/{job_name}.sh", "w") as f:
+        f.write("#!/bin/bash\n")
+        for command in commands:
+            f.write(f"{command}\n")
+    subprocess.run(["chmod", "+x", f"{condor_dir}/{job_name}.sh"])
+
+    # Create condor submission file
+    with open(f"{condor_dir}/{job_name}.sub", "w") as f:
+        f.write("getenv = true\n")
+        f.write(f"executable = {condor_dir}/{job_name}.sh\n")
+        f.write(f"output = {condor_dir}/{job_name}.$(Cluster).out\n")
+        f.write(f"error = {condor_dir}/{job_name}.$(Cluster).err\n")
+        f.write(f"log = {condor_dir}/{job_name}.$(Cluster).log\n")
+        f.write("request_cpus = 1\n")
+        f.write("request_memory = 2GB\n")
+        f.write("+MaxRuntime = 18000\n")  # Set max runtime to 5 hours
+        f.write("queue\n")
+
+    subprocess.run(["condor_submit", f"{condor_dir}/{job_name}.sub"])
--- scripts/nano_postproc.py
+++ scripts/nano_postproc.py
@@ -11,6 +11,7 @@ import json
 from summaryProducer import *
 from selectionFilter import *
 from tupleProducer import *
+from submitJob import condor_submit
 
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
@@ -22,6 +23,7 @@ if __name__ == "__main__":
     parser.add_argument('--output', required=True, type=str, help="Output directory")
     parser.add_argument('--isMC', action='store_true', help="is it MC?")
     parser.add_argument('--era', required=True, type=str, help="2022, 2023, 2024")
+    parser.add_argument('--batch', action='store_true', help="run in batch mode")
     args = parser.parse_args()
     print("args = ",args)
 
@@ -50,58 +52,87 @@ if __name__ == "__main__":
     if era == "2022EE": era = "2022"
     if era == "2023BPix": era = "2023"
 
-    if isMC:
-        jsoninput = None,
-        if era == '2016':
-            Modules = [summary2016MC(), selection2016MC(), tuple2016MC()]
-        elif era == '2017':
-            Modules = [summary2017MC(), selection2017MC(), tuple2017MC()]
-        elif era == '2018':
-            Modules = [summary2018MC(), selection2018MC(), tuple2018MC()]
-        elif era == '2022':
-            Modules = [summary2022MC(), selection2022MC(), tuple2022MC()]
-        elif era == '2023':
-            Modules = [summary2023MC(), selection2023MC(), tuple2023MC()]
-        elif era == '2024':
-            Modules = [summary2024MC(), selection2024MC(), tuple2024MC()]
+    if not args.batch:
+        if isMC:
+            jsoninput = None,
+            if era == '2016':
+                Modules = [summary2016MC(), selection2016MC(), tuple2016MC()]
+            elif era == '2017':
+                Modules = [summary2017MC(), selection2017MC(), tuple2017MC()]
+            elif era == '2018':
+                Modules = [summary2018MC(), selection2018MC(), tuple2018MC()]
+            elif era == '2022':
+                Modules = [summary2022MC(), selection2022MC(), tuple2022MC()]
+            elif era == '2023':
+                Modules = [summary2023MC(), selection2023MC(), tuple2023MC()]
+            elif era == '2024':
+                Modules = [summary2024MC(), selection2024MC(), tuple2024MC()]
+            else:
+                raise RuntimeError("Please check the right Year!")
+            p = PostProcessor(output, files, "1", 
+                              branchsel = "keep_and_drop.txt", 
+                              modules= Modules, 
+                              provenance=True,
+                              outputbranchsel = "output_branch.txt"
+            )
+
         else:
-            raise RuntimeError("Please check the right Year!")
-        p = PostProcessor(output, files, "1", 
-                        branchsel = "keep_and_drop.txt", 
-                        modules= Modules, 
-                        provenance=True,
-                        outputbranchsel = "output_branch.txt"
-        )
+            if era == '2022':
+                Modules = [summary2022data(), selection2022data(), tuple2022data()]
+                lumi_json_path = './lumi_jsons/2022.pkl' 
+            elif era == '2023':
+                Modules = [summary2023data(), selection2023data(), tuple2023data()]
+                lumi_json_path = './lumi_jsons/2023.pkl'
+            elif era == '2024':
+                Modules = [summary2024data(), selection2024data(), tuple2024data()]
+                lumi_json_path = './lumi_jsons/Cert_Collisions2024_378981_386951_Golden.json'
+            else:
+                raise RuntimeError("Please check the right Year!")
+
+            if lumi_json_path.endswith('.pkl'):
+                with open(lumi_json_path, 'rb') as file:
+                    jsoninput = pickle.load(file)
+            elif lumi_json_path.endswith('.json'):
+                with open(lumi_json_path, 'r') as file:
+                    jsoninput = json.load(file)
+
+            p = PostProcessor(output, files, "1", 
+                              branchsel = "keep_and_drop.txt", 
+                              modules= Modules, 
+                              jsonInput=jsoninput,
+                              provenance=True,
+                              outputbranchsel = "output_branch.txt"
+            )
+
+        p.run()
+        print("Done !")
 
     else:
-        if era == '2022':
-            Modules = [summary2022data(), selection2022data(), tuple2022data()]
-            lumi_json_path = './lumi_jsons/2022.pkl' 
-        elif era == '2023':
-            Modules = [summary2023data(), selection2023data(), tuple2023data()]
-            lumi_json_path = './lumi_jsons/2023.pkl'
-        elif era == '2024':
-            Modules = [summary2024data(), selection2024data(), tuple2024data()]
-            lumi_json_path = './lumi_jsons/Cert_Collisions2024_378981_386951_Golden.json'
+        BATCH_SIZE = 5  # Number of files to process in each batch
+        commands = []
+        if args.isMC:
+            for file in files:
+                commands.append(
+                    f"python3 scripts/nano_postproc.py --input {file} --isMC --era {args.era} --output {args.output}"
+                )
         else:
-            raise RuntimeError("Please check the right Year!")
-
-        if lumi_json_path.endswith('.pkl'):
-            with open(lumi_json_path, 'rb') as file:
-                jsoninput = pickle.load(file)
-        elif lumi_json_path.endswith('.json'):
-            with open(lumi_json_path, 'r') as file:
-                jsoninput = json.load(file)
-
-        p = PostProcessor(output, files, "1", 
-                        branchsel = "keep_and_drop.txt", 
-                        modules= Modules, 
-                        jsonInput=jsoninput,
-                        provenance=True,
-                        outputbranchsel = "output_branch.txt"
-        )
-
-    p.run()
-    print("Done !")
-
+            for file in files:
+                commands.append(
+                    f"python3 scripts/nano_postproc.py --input {file} --era {args.era} --output {args.output}"
+                )
+        
+        for i in range(0, len(commands), BATCH_SIZE):
+            batch_commands = commands[i:min(i + BATCH_SIZE, len(commands))]
+            if args.isMC:
+                job_name = f"nano_postproc_mc_{args.era}_{i//BATCH_SIZE}"
+            else:
+                job_name = f"nano_postproc_data_{args.era}_{i//BATCH_SIZE}"
+            print(
+                "\033[92m"
+                + f"Submitting job: {job_name}"
+                + "\033[0m"
+            )
+            condor_dir = f"{args.output}/condor"
+            os.makedirs(condor_dir, exist_ok=True)
+            condor_submit(batch_commands, condor_dir, job_name)
 
